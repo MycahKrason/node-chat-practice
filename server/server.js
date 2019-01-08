3e911c337b2bc2646f6552cfe5bc163b04ce04a6
Mycah <mycahkrason@gmail.com>
2019-01-07 19:22:12 -0800

    Set up welcome and notification when users join

--- server/server.js
+++ server/server.js
@@ -14,15 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    //Welcome the user
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the Chat',
+        createdAt: new Date().getTime()
+    });
+
+    //Alert everyone that someone has joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
+        // Send message to everyone
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
-
+        
     });
 
     socket.on('disconnect', () => {
