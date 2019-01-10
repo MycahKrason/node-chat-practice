de62284106403861eed803a8c61efb74619599cc
Mycah <mycahkrason@gmail.com>
2019-01-09 18:16:17 -0800

    created a message utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public/');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,28 +16,16 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     //Welcome the user
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the Chat',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcom to the chat'));
 
     //Alert everyone that someone has joined
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user has joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
         // Send message to everyone
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         
     });
 
